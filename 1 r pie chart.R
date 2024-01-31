84ee0906d8d29958ddb285e5c9d797d427cd01c0
kamini8888 <112712900+kamini8888@users.noreply.github.com>
2024-01-31 08:46:46 +0530

    Add files via upload

---
+++ 1 r pie chart.R
@@ -0,0 +1,10 @@
+
+# Creating data for the graph.  
+x <- c(20, 65, 15, 50)  
+labels <- c("India", "America", "Sri Lanka", "Nepal")  
+# Giving the chart file a name.  
+
+# Plotting the chart.  
+pie(x,labels)  
+# Saving the file.  
+
